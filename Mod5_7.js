b60efba22204356fca60f3a0e19dbb0b90f89a37
IRINA <76259525+irina-denisova@users.noreply.github.com>
2021-02-18 22:23:52 +0100

    Update Mod5_7.js
    Task 7 changed

--- Mod5_7.js
+++ Mod5_7.js
@@ -1,22 +1,24 @@
 // 7) Дан массив. Нужно вывести в консоль количество чётных и нечётных элементов в массиве. 
 // Если в массиве есть нулевой элемент, то он учитывается и выводится отдельно. 
 
-const arr7 = [2, 4, 7, 8, 10, null, 0, 5];
+const arr = [0, null, NaN, 0, 1, 2, 4, "str"];
 
 let odd = 0;
 let even = 0;
-let sumNull = 0;
+let zero = 0;
 
 
-arr7.forEach(function(item, index, array) {
-
-  if ( !item ) {
-    sumNull++;
-  } else if ( item % 2 === 0 ) {
-    even++;
-  } else { 
-    odd++;
+arr.forEach(item => {
+  
+  if ( Number.isFinite(item) ) {
+    
+    if  ( item === 0) {
+      zero++;
+    } else if ( item % 2 === 0 ) {  
+      even++; 
+    } else { odd++; } 
   }
-})
 
-console.log(`Odd: ${odd}, Even: ${even}, Null: ${sumNull}`);
+});
+
+console.log(`Odd: ${odd}, Even: ${even}, Zero: ${zero}`);
